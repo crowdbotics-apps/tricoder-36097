af405e07184050dd791266bf361401713b2ffd0c
Crowdbotics <team@crowdbotics.com>
2022-07-08 14:13:11 +0000

    Update models

--- backend/tricoder_36097/wsgi.py
+++ backend/tricoder_36097/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tricoder_36097.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tricoder_36097.settings")
 
 application = get_wsgi_application()
