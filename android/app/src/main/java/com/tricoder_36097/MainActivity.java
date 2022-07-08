f3858f91028b4cfc7ccf4d60a30ef2e07f340e25
Crowdbotics <team@crowdbotics.com>
2022-07-08 14:10:56 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/tricoder_36097/MainActivity.java
@@ -0,0 +1,15 @@
+package com.tricoder_36097;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "tricoder_36097";
+  }
+}
